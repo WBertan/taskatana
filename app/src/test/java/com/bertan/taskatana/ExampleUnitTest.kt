43970fe6037adeb1dcdefa8ee71e72df2a22b646
WBertan <william.bertan@gmail.com>
2018-05-28 11:56:16 +0100

    PR Self-review.

--- app/src/test/java/com/bertan/taskatana/ExampleUnitTest.kt
+++ app/src/test/java/com/bertan/taskatana/ExampleUnitTest.kt
@@ -12,23 +12,6 @@ import java.util.*
 class ExampleUnitTest {
   @Test
   fun addition_isCorrect() {
-    val t1 = Task(UUID.randomUUID(), "1", false, 1)
-    val t2 = Task(UUID.randomUUID(), "2", false, 2)
-    val t3 = Task(UUID.randomUUID(), "3", false, 3)
-    val asas = mutableListOf(t1, t2, t3)
-
-    val toAdd = Task(UUID.randomUUID(), "4", false, 4)
-
-    val index = asas.indexOf(toAdd)
-    when (index) {
-      -1 -> asas.add(toAdd)
-      else -> asas.set(index, toAdd)
-    }
-
-    asas.forEach {
-      println("$it")
-    }
-
     assertEquals(4, 2 + 2)
   }
 }
